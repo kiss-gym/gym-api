7c7d9c273da6d4505d4531e6cf7bbe1defe89688
zan-igr <edanziger@t-online.de>
2026-05-15 12:07:40 +0200

    UserId removed from CreateSessionRequest

--- GymApi.Api/Models/Requests/CreateSessionRequest.cs
+++ GymApi.Api/Models/Requests/CreateSessionRequest.cs
@@ -1,8 +1,5 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace GymApi.Api.Models.Requests;
 
 public sealed record CreateSessionRequest(
-    [Required] Guid UserId,
     string? Label = null,
     Guid? InheritFromSessionId = null);
